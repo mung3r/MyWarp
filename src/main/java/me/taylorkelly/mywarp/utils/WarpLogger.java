af13f21970498553c700aec78ce2e7f6a8125378
mung3r <rodney@bighatchet.com>
2012-08-04 10:31:01 -0700

    minor refactor on logger

--- src/main/java/me/taylorkelly/mywarp/utils/WarpLogger.java
+++ src/main/java/me/taylorkelly/mywarp/utils/WarpLogger.java
@@ -5,21 +5,26 @@ import java.util.logging.Logger;
 
 public class WarpLogger {
 
-    public static final Logger log = Logger.getLogger("Minecraft");
+    private static String name = "MyWarp";
+    private static final Logger log = Logger.getLogger("Minecraft");
 
     public static void severe(String string, Exception ex) {
-        log.log(Level.SEVERE, "[MyWarp] " + string, ex);
+        log.log(Level.SEVERE, format(string), ex);
     }
 
     public static void severe(String string) {
-        log.log(Level.SEVERE, "[MyWarp] " + string);
+        log.severe(format(string));
     }
 
     public static void info(String string) {
-        log.log(Level.INFO, "[MyWarp] " + string);
+        log.info(format(string));
     }
 
     public static void warning(String string) {
-        log.log(Level.WARNING, "[MyWarp] " + string);
+        log.warning(format(string));
+    }
+
+    public static String format(String msg) {
+        return String.format("[%s] %s", name, msg);
     }
 }
