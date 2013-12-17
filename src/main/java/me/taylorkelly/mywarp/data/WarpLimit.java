dc125dfefc6ad1c1939dac7753a9609826cfacb5
TheE <thee@online.de>
2013-12-17 22:28:45 +0100

    BRAKING CHANGE: Made limits work per world or per multiple worlds While this change could have been done without braking existing setups, I think it is the perfect point to fix some ugly stuff: configured-limits now have their own configuration-section, permissions are in line with the timer's 'disobey' syntax.
    The new concept should still be easy to use for global setups, but also
    covers more complex approaches. The default-limit is forced to be
    global, other limits can be configured to match only warps on certain
    worlds. Players can disobey limits per type and world, per world only,
    per type only, and completely. All permissions use internal parent/child
    structure so there is no overhead when checking on creation.

--- src/main/java/me/taylorkelly/mywarp/data/WarpLimit.java
+++ src/main/java/me/taylorkelly/mywarp/data/WarpLimit.java
@@ -1,5 +1,11 @@
 package me.taylorkelly.mywarp.data;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+import me.taylorkelly.mywarp.MyWarp;
 import me.taylorkelly.mywarp.utils.ValuePermissionContainer;
 
 /**
@@ -11,6 +17,7 @@ public class WarpLimit extends ValuePermissionContainer {
     private final int maxTotal;
     private final int maxPublic;
     private final int maxPrivate;
+    private final List<String> worlds;
 
     /**
      * Initializes the WarpLimit. The given limits are stored internally.
@@ -24,11 +31,14 @@ public class WarpLimit extends ValuePermissionContainer {
      * @param maxPrivate
      *            the private warp-limit
      */
-    public WarpLimit(String name, int maxTotal, int maxPublic, int maxPrivate) {
+    public WarpLimit(String name, int maxTotal, int maxPublic, int maxPrivate, List<String> worlds) {
         super(name);
         this.maxTotal = maxTotal;
         this.maxPublic = maxPublic;
         this.maxPrivate = maxPrivate;
+        this.worlds = worlds;
+        
+        MyWarp.logger().info("Initated warp-limit: " + name + "(maxTotal: " + maxTotal + ", maxPublic: " + maxPublic + ", maxPrivate: " + maxPrivate + ", worlds: " + StringUtils.join(worlds, ";") + ")"); 
     }
 
     /**
@@ -56,5 +66,17 @@ public class WarpLimit extends ValuePermissionContainer {
      */
     public int getMaxPrivate() {
         return maxPrivate;
+    }
+
+    public List<String> getEffectiveWorlds() {
+        return Collections.unmodifiableList(worlds);
+    }
+    
+    public boolean isGlobal() {
+        return worlds.contains("all");
+    }
+    
+    public boolean isEffectiveWorld (String worldname) {
+        return isGlobal() || worlds.contains(worldname);
     }
 }
